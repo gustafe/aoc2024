25ef9ed4303d709608646c7e7d90c98be594afbc
Gustaf Erikson <gerikson@gmail.com>
2024-12-20 10:19:50 +0100

    added solution for day 12

--- d12-Garden-Groups.pl
+++ d12-Garden-Groups.pl
@@ -5,7 +5,8 @@
 ###########################################################
 use Modern::Perl '2015';
 # useful modules
-use List::Util qw/sum/;
+use List::Util qw/sum min max/;
+use Array::Utils qw(:all);
 use Data::Dump qw/dump/;
 use Test::More;
 use Time::HiRes qw/gettimeofday tv_interval/;
@@ -35,10 +36,10 @@ for my $line (@input) {
 # flood fill with different areas
 my $area_id = 1;
 my %dirs = ( N => [ -1, 0 ], S => [ 1, 0 ], W => [ 0, -1 ], E => [ 0, 1 ] );
+
 for my $R ( sort { $a <=> $b } keys %$Map ) {
     for my $C ( sort { $a <=> $b } keys %{ $Map->{$R} } ) {
         next if $Map->{$R}{$C}{area_id};    # already seen and filled
-
         my $label = $Map->{$R}{$C}{label};
         $Map->{$R}{$C}{area_id} = $area_id;
         my @stack = ( [ $R, $C ] );
@@ -68,42 +69,101 @@ for my $R ( sort { $a <=> $b } keys %$Map ) {
 my $count;
 my $border_cells;
 
-#my $corners;
 for my $r ( sort { $a <=> $b } keys %$Map ) {
     for my $c ( sort { $a <=> $b } keys %{ $Map->{$r} } ) {
+
+        # gather different areas into their own maps
+
         $count->{ $Map->{$r}{$c}{label} }{ $Map->{$r}{$c}{area_id} }{area}++
             if $Map->{$r}{$c}{area_id};
         if ( $Map->{$r}{$c}{border} ) {
-            if ( $testing and $Map->{$r}{$c}{area_id} == 5 ) {
-                print STDERR "$r,$c: ";
-                dump $Map->{$r}{$c};
-
-            }
 
             # part 1
             $count->{ $Map->{$r}{$c}{label} }{ $Map->{$r}{$c}{area_id} }
                 {perimeter} += scalar keys %{ $Map->{$r}{$c}{border} };
+
+            # part 2
+            $border_cells->{ $Map->{$r}{$c}{area_id} }{$r}{$c}
+                = $Map->{$r}{$c}{border};
         }
 
     }
 }
+
 my $sum = 0;
 
+#my $sum2=0;
 for my $label ( keys %$count ) {
 
     for my $area_id ( keys %{ $count->{$label} } ) {
-
         $sum += $count->{$label}{$area_id}{area}
             * $count->{$label}{$area_id}{perimeter};
     }
 }
 
+# for each dimension, move through the grid and gather all cells which
+# have a border facing in a certain direction. Gather those per
+# area_id
+
+my $sides;
+for my $r ( sort { $a <=> $b } keys %$Map ) {
+    for my $c ( sort { $a <=> $b } keys %{ $Map->{$r} } ) {
+        for my $d ( 'N', 'S' ) {
+
+            if ( $Map->{$r}{$c}{border}{$d} ) {
+                push @{ $sides->{$d}{$r}{ $Map->{$r}{$c}{area_id} } }, $c;
+            }
+        }
+        for my $d ( 'E', 'W' ) {
+            if ( $Map->{$r}{$c}{border}{$d} ) {
+                push @{ $sides->{$d}{$c}{ $Map->{$r}{$c}{area_id} } }, $r;
+            }
+        }
+    }
+}
+
+# for each row / column, count consecutive cells facing in the same
+# direction. Each consecutive run counts as a side for that particular
+# area
+
+my $collate;
+for my $v ( keys %{$sides} ) {
+    for my $dim ( keys %{ $sides->{$v} } ) {
+        for my $id ( keys %{ $sides->{$v}{$dim} } ) {
+            my %els = map { $_ => 1 } @{ $sides->{$v}{$dim}{$id} };
+            my ( $start, $end ) = ( min( keys %els ), max( keys %els ) );
+
+            my $consecutive = 0;
+            my $runs        = 0;
+            for my $idx ( $start .. $end ) {
+                if ( !$els{$idx} and $consecutive ) {
+                    $consecutive = 0;
+                }
+                if ( $els{$idx} and !$consecutive ) {
+                    $consecutive = 1;
+                    $runs++;
+                }
+            }
+            $collate->{$id} += $runs;
+        }
+    }
+}
+
+my $sum2;
+for my $label ( keys %$count ) {
+    for my $area_id ( keys %{ $count->{$label} } ) {
+        $sum2 += $count->{$label}{$area_id}{area} * $collate->{$area_id};
+    }
+}
+
 ### FINALIZE - tests and run time
 if ( !$testing ) {
-    is( $sum, 1446042, "Part 1: $sum" );
+    is( $sum,  1446042, "Part 1: $sum" );
+    is( $sum2, 902742,  "Part 2: $sum2" );
 } else {
 
-    is( $sum, 1930, "Part 1: $sum" );
+    is( $sum,  1930, "Part 1: $sum" );
+    is( $sum2, 1206, "Part 2: $sum2" );
 }
 
 done_testing();
@@ -124,15 +184,18 @@ sub sec_to_hms {
 
 =encoding utf8
 
-Part 1 only for now.
+This took me a few days - not that I worked on it full time, but in
+the times between other puzzles.
+
 
 I use flood-fill to fill out the different areas, and at the same time
 I mark out if I find a border and in which direction it's facing. That
-helped me find the perimiter, and is a good start for finding sides
-(or rather corners), but I still need to figure out all the literal
-edge cases.
+helped me find the perimiter, and is a good start for finding sides.
 
-Score: 1
+After messing around trying to identify corners, I did what everyone
+else did and "sorted" sides per row and column.
+
+Score: 2
 
 Leaderboard completion time: 17m42s
 
